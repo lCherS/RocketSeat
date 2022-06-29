349f500b800d6f5588dfe7ee94188c1fa7432e6c
lCherS <lucascherez.cl@gmail.com>
2022-06-29 12:50:43 -0300

    Finalizando EJS

--- Discover/Fundamentar/NodeJS/EJS/server.js
+++ Discover/Fundamentar/NodeJS/EJS/server.js
@@ -5,11 +5,39 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const items = [
+        {
+            title: 'D',
+            message: 'esenvolvendo app Node + EJS'
+        },
+        {
+            title: 'E',
+            message: 'JS na base de JS para HTML'
+        },
+        {
+            title: 'M',
+            message: 'o facin usa-lo'
+        },
+        {
+            title: 'A',
+            message: 'prende-se rapidinho'
+        },
+        {
+            title: 'I',
+            message: 'ntuitivo e facil'
+        },
+        {
+            title: 'S',
+            message: 'uper convencional e belezinha'
+        }
+    ]
+    res.render('pages/index', {
+        qualitys: items
+    });
 })
 
 app.get('/sobre', (req, res) => {
-    res.render('about');
+    res.render('pages/about');
 })
 
 app.listen(8080);
