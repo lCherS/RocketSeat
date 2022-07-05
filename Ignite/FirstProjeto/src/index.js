54d9ee96c82d3f138b134cf8fbddc83809edecdd
lCherS <lucascherez.cl@gmail.com>
2022-07-04 23:38:25 -0300

    Validando, listando e middlewares

--- Ignite/FirstProjeto/src/index.js
+++ Ignite/FirstProjeto/src/index.js
@@ -7,17 +7,48 @@ const customers = [];
 
 app.use(express.json());
 
+// Middlewares
+
+function VerifyExistsACcountCPF(req, res, next) {
+  const { cpf } = req.headers;
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if(!customer) {
+    return res.status(400).send({ error: "Customer not found" })
+  }
+
+  req.customer = customer;
+
+  return next();
+}
+
 app.post('/account', (req, res) => {
   const { cpf, name } = req.body;
-  const id = uuidv4();
 
+  const customersAlreadyExist = customers.some(
+    (customer) => customer.cpf === cpf)
+
+  if(customersAlreadyExist) {
+    return res.status(400).send({
+      error: "Customer already exists"
+    })
+  }
   customers.push({
     cpf,
     name,
-    id,
+    id: uuidv4(),
     statement: []
   })
 
-  return res.status(201).send();
+  return res.status(201).send(customers.name);
 })
+
+app.get('/statement/', VerifyExistsACcountCPF, (req, res) => {
+  //  const { cpf } = req.params;
+  //  const { cpf } = req.headers; // ao invez de usar params, podemos utilizar o headers
+  const { customer } = req;
+
+  return res.send(customer);
+})
+
 app.listen(3333);
