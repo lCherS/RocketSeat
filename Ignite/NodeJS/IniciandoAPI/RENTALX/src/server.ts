1c90305a404f26f5b1350673b73d3e2390f54164
lCherS <lucascherez.cl@gmail.com>
2022-07-06 01:53:31 -0300

    Criando Debug, categoria e ID

--- Ignite/NodeJS/IniciandoAPI/RENTALX/src/server.ts
+++ Ignite/NodeJS/IniciandoAPI/RENTALX/src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
 
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  console.log('hello dark')
-})
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => { console.log('rodnado')});
