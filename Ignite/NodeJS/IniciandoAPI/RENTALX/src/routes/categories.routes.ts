54292d929d752dbfefbd28e90811d363da309ca6
lCherS <lucascherez.cl@gmail.com>
2022-07-06 22:39:32 -0300

    Criando, listando e validando categorias

--- Ignite/NodeJS/IniciandoAPI/RENTALX/src/routes/categories.routes.ts
+++ Ignite/NodeJS/IniciandoAPI/RENTALX/src/routes/categories.routes.ts
@@ -1,21 +1,29 @@
-import { Router } from 'express';
+import { response, Router } from 'express';
 
 import { Category } from '../model/Category';
+import { CategoryRepository } from '../repositories/Categories.Repository';
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoryRepository();
 
 categoriesRoutes.post("/", (req, res) => {
   const {name, description} = req.body;
 
-  const category = new Category();
-  
-  Object.assign(category, {name, description, create_at: new Date()});
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
 
-  categories.push(category);
+  if (categoryAlreadyExists) {
+    return res.status(400).json({ error: 'category already exists'});
+  }
 
-  return res.status(201).json({category});
+    categoriesRepository.create({name, description});
+
+  return res.status(201).send(`${name} , ${description}`);
+})
+
+categoriesRoutes.get('/', (req, res) => {
+
+  const all = categoriesRepository.list();
+  return res.json(all)
 })
 
 export { categoriesRoutes }
