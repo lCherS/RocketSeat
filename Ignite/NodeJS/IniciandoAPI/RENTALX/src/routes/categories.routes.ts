f2936085ef15a8d1178f7bfabea64434273db286
lCherS <lucascherez.cl@gmail.com>
2022-07-18 10:02:39 -0300

    Upload e UseCase com Multer

--- Ignite/NodeJS/IniciandoAPI/RENTALX/src/routes/categories.routes.ts
+++ Ignite/NodeJS/IniciandoAPI/RENTALX/src/routes/categories.routes.ts
@@ -1,12 +1,22 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
 import { listCategoriesController } from '../modules/cars/useCases/ListCategories';
+
+const upload = multer({
+  dest: './tmp'
+})
 
 const categoriesRoutes = Router();
 
 categoriesRoutes.post("/", (req, res) => {
   return createCategoryController.handle(req, res);
+})
+
+categoriesRoutes.post('/import', upload.single('file'), (req, res) => {
+  return importCategoryController.handle(req, res);
 })
 
 categoriesRoutes.get('/', (req, res) => {
