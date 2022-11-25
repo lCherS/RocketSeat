b85e3b2248cc8054e74b5c85f605763c0b933071
lCherS <lucascherez.cl@gmail.com>
2022-11-25 00:22:42 -0300

    Aula 2 - Adicionando validaçoes de datas

--- GoStack/Nivel2/PrimeiroProjeto/src/routes/appointments.routes.ts
+++ GoStack/Nivel2/PrimeiroProjeto/src/routes/appointments.routes.ts
@@ -1,16 +1,35 @@
 import { Router } from "express";
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+
+interface appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
 const appointmentsRouter = Router();
 
-const appointments = [];
+const appointments: appointment[] = new Array();
 
 appointmentsRouter.post('/', (req, res) => {
   const { provider, date } = req.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find(appointment => isEqual(appointment.date,
+    parsedDate));
+
+  if (findAppointmentInSameDate) {
+    return res.status(400).json({
+      message: 'Appointment already exists'
+    })
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
