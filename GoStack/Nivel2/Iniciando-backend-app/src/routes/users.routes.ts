fb35d611a5eb6f0ce2202b76dcb0001919b28e89
lCherS <lucascherez.cl@gmail.com>
2023-01-09 13:43:43 -0300

    adicionando uploads com multer

--- GoStack/Nivel2/Iniciando-backend-app/src/routes/users.routes.ts
+++ GoStack/Nivel2/Iniciando-backend-app/src/routes/users.routes.ts
@@ -1,13 +1,16 @@
-import { Router } from "express";
+import { response, Router } from "express";
 import { getCustomRepository } from "typeorm";
 import { parseISO } from 'date-fns';
+import multer from 'multer';
 
+import uploadConfig from "../config/upload";
 import CreateUserService from "../services/CreateUserService";
+import ensureAuthenticated from "../middlewares/ensureAuthenticated";
 
 // Uma rota deve apenas ser responsavel por receber os dados, chamar outro arquivo para tratar, e devolver uma resposta
 
 const usersRouter = Router();
-
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (req, res) => {
   try {
@@ -27,6 +30,13 @@ usersRouter.post('/', async (req, res) => {
   } catch (e: any) {
     return res.status(400).json({ error: e.message })
   }
+})
+
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (req, res) => {
+
+
+
+  return res.json({ ok: true})
 })
 
 export default usersRouter;
