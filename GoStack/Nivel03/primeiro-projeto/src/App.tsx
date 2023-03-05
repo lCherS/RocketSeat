0de144ff09dbb8014b7231543d6b35f8418700ac
lCherS <lucascherez.cl@gmail.com>
2023-03-05 16:32:25 -0300

    estilizando

--- GoStack/Nivel03/primeiro-projeto/src/App.tsx
+++ GoStack/Nivel03/primeiro-projeto/src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyle from './styles/global'
 import Rota from  './routes';
 
  const App: React.FC = () => (
+  <>
   <BrowserRouter>
   <Rota />
   </BrowserRouter>
+  <GlobalStyle />
+  </>
 )
 
 export default App
--- GoStack/Nivel03/primeiro-projeto/src/pages/Dashboard/index.tsx
+++ GoStack/Nivel03/primeiro-projeto/src/pages/Dashboard/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-
+import {Title} from './styles'
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>
+  return <Title>Explore Repositorio no Github</Title>
 }
 
 export default Dashboard;
