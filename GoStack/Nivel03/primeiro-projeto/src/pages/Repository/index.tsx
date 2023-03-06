dd7c86914619f8e4bac8c000c10ea26e851398ac
lCherS <lucascherez.cl@gmail.com>
2023-03-06 10:57:19 -0300

    finalizando estilização da pagian repository

--- GoStack/Nivel03/primeiro-projeto/src/pages/Repository/index.tsx
+++ GoStack/Nivel03/primeiro-projeto/src/pages/Repository/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { matchRoutes, useMatch, useResolvedPath, useLocation, useParams } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Link, useLocation } from 'react-router-dom';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+const logoImg = 'https://xesque.rocketseat.dev/platform/1587379765556-attachment.svg'
+
 
 interface RepositoryParams {
   repository: string;
@@ -7,7 +13,47 @@ interface RepositoryParams {
 
 const Repositories: React.FC = () => {
   const repository = useLocation().pathname.replace('/repositories', '');
-  return (<h1>Repository: {repository}</h1>)
+  return (
+    <>
+    <Header>
+      <img src={logoImg} alt="Logo GitHub Explorer" />
+      <Link to="/"> <FiChevronLeft size={16} /> Voltar</Link>
+    </Header>
+
+    <RepositoryInfo>
+      <header>
+        <img src="" alt="Foto perfil github" />
+        <div>
+          <strong></strong>
+          <p></p>
+        </div>
+      </header>
+      <ul>
+      <li>
+          <strong>12</strong>
+          <span>Stars</span>
+        </li>
+        <li>
+          <strong>4</strong>
+          <span>Forks</span>
+        </li>
+        <li>
+          <strong>3</strong>
+          <span>Issues Abertas</span>
+        </li>
+      </ul>
+    </RepositoryInfo>
+    <Issues>
+    <Link to='asas'>
+      <div>
+        <strong>zca</strong>
+        <p>asdsa</p>
+      </div>
+      <FiChevronRight size={20} />
+    </Link>
+    </Issues>
+    </>
+  )
 }
 
 export default Repositories;
