39026e7a2bab820abe7c8dda16ccea26f7eeadc2
lCherS <lucascherez.cl@gmail.com>
2023-03-06 11:29:02 -0300

    finalizado projeto

--- GoStack/Nivel03/primeiro-projeto/src/pages/Repository/index.tsx
+++ GoStack/Nivel03/primeiro-projeto/src/pages/Repository/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 import { Link, useLocation } from 'react-router-dom';
+import api from '../../services/api';
 
 import { Header, RepositoryInfo, Issues } from './styles';
 
@@ -10,9 +11,45 @@ const logoImg = 'https://xesque.rocketseat.dev/platform/1587379765556-attachment
 interface RepositoryParams {
   repository: string;
 }
+interface Repository {
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
 
+  }
+}
+
+interface issues {
+  title: string;
+  id: number;
+  html_url: string;
+  user: {
+    login: string;
+  }
+}
 const Repositories: React.FC = () => {
+  const [repositorio, setRepositorio] = useState<Repository | null>(null)
+  const [issues, setIssues] = useState<issues[]>([])
   const repository = useLocation().pathname.replace('/repositories', '');
+  
+  useEffect(() => {
+    api.get(`repos${repository}`).then(response => {
+      setRepositorio(response.data)
+    })
+
+    api.get(`repos${repository}/issues`).then(response => {
+      console.log(response.data);
+      setIssues(response.data);
+    })
+
+  }, [repository])
+
+  console.log(issues)
   return (
     <>
     <Header>
@@ -20,38 +57,45 @@ const Repositories: React.FC = () => {
       <Link to="/"> <FiChevronLeft size={16} /> Voltar</Link>
     </Header>
 
-    <RepositoryInfo>
-      <header>
-        <img src="" alt="Foto perfil github" />
-        <div>
-          <strong></strong>
-          <p></p>
-        </div>
-      </header>
-      <ul>
-      <li>
-          <strong>12</strong>
-          <span>Stars</span>
-        </li>
-        <li>
-          <strong>4</strong>
-          <span>Forks</span>
-        </li>
-        <li>
-          <strong>3</strong>
-          <span>Issues Abertas</span>
-        </li>
-      </ul>
-    </RepositoryInfo>
-    <Issues>
-    <Link to='asas'>
+    { repositorio && (
+          <RepositoryInfo>
+          <header>
+            <img src={repositorio.owner.avatar_url} alt="Foto perfil github" />
+            <div>
+              <strong>{repositorio.full_name}</strong>
+              <p>{repositorio.description}</p>
+            </div>
+          </header>
+          <ul>
+          <li>
+              <strong>{repositorio.stargazers_count}</strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong>{repositorio.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>{repositorio.open_issues_count}</strong>
+              <span>Issues Abertas</span>
+            </li>
+          </ul>
+        </RepositoryInfo>
+    )
+
+    }
+
+  <Issues>
+    {issues.map(issue => (
+    <a key={issue.id} href={issue.html_url}>
       <div>
-        <strong>zca</strong>
-        <p>asdsa</p>
+        <strong>{issue.title}</strong>
+        <p>{issue.user.login}</p>
       </div>
       <FiChevronRight size={20} />
-    </Link>
-    </Issues>
+    </a>
+    ))}
+  </Issues>
     </>
   )
 }
