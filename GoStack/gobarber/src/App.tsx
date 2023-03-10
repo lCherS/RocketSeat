0317386c05d8f02b7c96480362317aa5ea19e40e
lCherS <lucascherez.cl@gmail.com>
2023-03-10 10:45:47 -0300

    iniciando projeto GoBarber

---
+++ GoStack/gobarber/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global'
+function App() {
+  return (
+    <>
+    <h1>Logo</h1>
+    <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
