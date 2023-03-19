9db067b0b305912f1183efaecaa7b754ac8866bf
lCherS <lucascherez.cl@gmail.com>
2023-03-19 11:24:34 -0300

    alterando estrutura basica

---
+++ Discover/Especializar/reactapp/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function Home() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>Lista de Presença</h1>
+  )
+}
+
+export default Home
